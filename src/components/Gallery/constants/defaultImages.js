66990ef89467b91a4d9e8eca1d43be0c2c749027
Teo Cherici <teo@promaton.com>
2021-10-04 17:18:18 +0200

    Adds personal info

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -4,12 +4,12 @@ import full03 from '../../../assets/images/fulls/03.jpg'
 import full04 from '../../../assets/images/fulls/04.jpg'
 import full05 from '../../../assets/images/fulls/05.jpg'
 import full06 from '../../../assets/images/fulls/06.jpg'
-import thumb02 from '../../../assets/images/thumbs/02.jpg'
-import thumb01 from '../../../assets/images/thumbs/01.jpg'
-import thumb03 from '../../../assets/images/thumbs/03.jpg'
-import thumb04 from '../../../assets/images/thumbs/04.jpg'
-import thumb05 from '../../../assets/images/thumbs/05.jpg'
-import thumb06 from '../../../assets/images/thumbs/06.jpg'
+import thumb01 from '../../../assets/images/fulls/01.jpg'
+import thumb02 from '../../../assets/images/fulls/02.jpg'
+import thumb03 from '../../../assets/images/fulls/03.jpg'
+import thumb04 from '../../../assets/images/fulls/04.jpg'
+import thumb05 from '../../../assets/images/fulls/05.jpg'
+import thumb06 from '../../../assets/images/fulls/06.jpg'
 
 
 export const DEFAULT_IMAGES = [
@@ -17,42 +17,42 @@ export const DEFAULT_IMAGES = [
       id: '1',
       source: full01,
       thumbnail: thumb01,
-      caption: 'Photo 1',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      caption: 'Me and my cat',
+      description: "She doesn't like it when I pick her up.",
     },
     {
       id: '2',
       source: full02,
       thumbnail: thumb02,
-      caption: 'Photo 2',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      caption: 'Tough kitty life',
+      description: 'To be or not to be fat.',      
     },
     {
       id: '3',
       source: full03,
       thumbnail: thumb03,
-      caption: 'Photo 3',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      caption: "Miss Poepie 'o Malley",
+      description: 'Loves bellyrubs.',
     },
     {
       id: '4',
       source: full04,
       thumbnail: thumb04,
-      caption: 'Photo 4',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      caption: 'Always comfy',
+      description: 'Such is the cat life.',
     },
     {
       id: '5',
       source: full05,
       thumbnail: thumb05,
-      caption: 'Photo 5',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      caption: 'Sits like a queen',
+      description: '(Im)patiently waiting for food.',
     },
     {
       id: '6',
       source: full06,
       thumbnail: thumb06,
-      caption: 'Photo 6',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      caption: 'After dinner',
+      description: 'Eat, sleep, poop, repeat.',
     }
 ]
