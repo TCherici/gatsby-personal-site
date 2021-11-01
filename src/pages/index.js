37cb631d67be5a46e1db87c8bd4d30a0a9dcbde3
Teo Cherici <teo@promaton.com>
2021-11-01 22:06:23 +0100

    Imports new CV

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,46 +45,11 @@ const HomeIndex = () => {
           </ul>
         </section>
         <section id="contact">
-          <h2>Get In Touch</h2>
+          <h2>Where to find me</h2>
           <p>
             I prefer contact through email.
           </p>
           <div className="row">
-            <div className="8u 12u$(small)">
-              {/* <form method="post" action="#">
-                <div className="row uniform 50%">
-                  <div className="6u 12u$(xsmall)">
-                    <input
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder="Name"
-                    />
-                  </div>
-                  <div className="6u 12u$(xsmall)">
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="12u">
-                    <textarea
-                      name="message"
-                      id="message"
-                      placeholder="Message"
-                      rows="4"
-                    ></textarea>
-                  </div>
-                </div>
-                <ul className="actions" style={{ marginTop: 30 }}>
-                  <li>
-                    <input type="submit" value="Send Message" />
-                  </li>
-                </ul>
-              </form> */}
-            </div>
             <div className="4u 12u$(small)">
               <ul className="labeled-icons">
                 <li>
@@ -95,16 +60,22 @@ const HomeIndex = () => {
                 tcherici@gmail.com
                 </li>
                 <li>
-                  <h3 className="icon fa-mobile">
-                    <span className="label">Phone</span>
+                  <h3 className="icon fa-github">
+                    <span className="label">GitHub</span>
                   </h3>
-                  +31 (0)641883688
+                  <a href="https://github.com/TCherici"> TCherici on github </a>
+                </li>
+                <li>
+                  <h3 className="icon fa-linkedin">
+                    <span className="label">LinkedIn</span>
+                  </h3>
+                  <a href="https://www.linkedin.com/in/teo-cherici-a6a6509b/">LinkedIn</a>
                 </li>
                 <li>
                   <h3 className="icon fa-home">
                     <span className="label">Address</span>
                   </h3>
-                  Leiden
+                  Leiden,
                   <br />
                   Netherlands
                 </li>
